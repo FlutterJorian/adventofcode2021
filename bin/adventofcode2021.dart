57de6946c23d189ce2693bfe4dfa5e9e26b5a0b6
Jorian <jorian@iconica.nl>
2021-12-03 16:40:10 +0100

    day3

--- bin/adventofcode2021.dart
+++ bin/adventofcode2021.dart
@@ -1,6 +1,7 @@
 import 'day1/day1.dart';
 import 'day2/day2.dart';
+import 'day3/day3.dart';
 
 void main() async {
-  day2();
+  day3();
 }
---
+++ bin/day3/day3.dart
@@ -0,0 +1,160 @@
+import 'dart:io';
+
+String findUnCommonV2(List<String> report) {
+  int ones = 0;
+  int zeros = 0;
+
+  int len = report[0].length;
+  for (int x = 0; x < len; x++) {
+    var aa = numLeft(report);
+    if (aa.isNotEmpty) {
+      return aa;
+    }
+    for (var num in report) {
+      if (num.isNotEmpty) {
+        if (num[x] == '1') {
+          ones++;
+        } else {
+          zeros++;
+        }
+      }
+    }
+    if (ones < zeros) {
+      for (int i = 0; i < report.length; i++) {
+        if (report[i].isNotEmpty && report[i][x] == '0') {
+          report[i] = '';
+        }
+      }
+    } else if (ones >= zeros) {
+      for (int i = 0; i < report.length; i++) {
+        if (report[i].isNotEmpty && report[i][x] == '1') {
+          report[i] = '';
+        }
+      }
+    }
+    ones = 0;
+    zeros = 0;
+  }
+  return numLeft(report);
+}
+
+String findCommonV2(List<String> report) {
+  int ones = 0;
+  int zeros = 0;
+
+  int len = report[0].length;
+  for (int x = 0; x < len; x++) {
+    var aa = numLeft(report);
+    if (aa.isNotEmpty) {
+      return aa;
+    }
+    for (var num in report) {
+      if (num.isNotEmpty) {
+        if (num[x] == '1') {
+          ones++;
+        } else {
+          zeros++;
+        }
+      }
+    }
+    if (ones >= zeros) {
+      for (int i = 0; i < report.length; i++) {
+        if (report[i].isNotEmpty && report[i][x] == '0') {
+          report[i] = '';
+        }
+      }
+    } else if (ones < zeros) {
+      for (int i = 0; i < report.length; i++) {
+        if (report[i].isNotEmpty && report[i][x] == '1') {
+          report[i] = '';
+        }
+      }
+    }
+    ones = 0;
+    zeros = 0;
+  }
+  return numLeft(report);
+}
+
+String numLeft(List<String> r) {
+  int a = 0;
+  String l = '';
+  for (var i in r) {
+    if (i.isNotEmpty) {
+      a++;
+      l = i;
+    }
+  }
+  if (a == 1) return l;
+  return '';
+}
+
+String findCommon(List<String> report) {
+  int ones = 0;
+  int zeros = 0;
+  String result = '';
+
+  int len = report[0].length;
+  for (int i = 0; i < len; i++) {
+    for (var num in report) {
+      if (num[i] == '1') {
+        ones++;
+      } else {
+        zeros++;
+      }
+    }
+    if (ones > zeros) {
+      result += '1';
+    } else {
+      result += '0';
+    }
+    ones = 0;
+    zeros = 0;
+  }
+  return result;
+}
+
+String findUnCommon(List<String> report) {
+  int ones = 0;
+  int zeros = 0;
+  String result = '';
+
+  int len = report[0].length;
+  for (int i = 0; i < len; i++) {
+    for (var num in report) {
+      if (num[i] == '1') {
+        ones++;
+      } else {
+        zeros++;
+      }
+    }
+    if (ones < zeros) {
+      result += '1';
+    } else {
+      result += '0';
+    }
+    ones = 0;
+    zeros = 0;
+  }
+  return result;
+}
+
+Future<List<String>> getDiagnosticReport() async {
+  var file = File('./bin/day3/input');
+  var lines = await file.readAsLines();
+
+  return lines;
+}
+
+void day3() async {
+  var report = await getDiagnosticReport();
+  // var gamma = findCommon(report);
+  // var epsilon = findUnCommon(report);
+//  var r = int.parse(gamma, radix: 2) * int.parse(epsilon, radix: 2);
+  var oxy = findCommonV2(report);
+  report = await getDiagnosticReport();
+  var scrub = findUnCommonV2(report);
+  var r = int.parse(oxy, radix: 2) * int.parse(scrub, radix: 2);
+//4790390
+  print(r);
+}
