7091dbd33dd63604ad4ebf58694b618b7abaa7df
Jorian <jorian@iconica.nl>
2021-12-16 15:53:54 +0100

    started day15

--- bin/day15/day15.dart
+++ bin/day15/day15.dart
@@ -1,9 +1,24 @@
 import 'dart:io';
 
-List<String> getInput() {
-  return File('day15/input.txt').readAsLinesSync();
+int safestRoute(List<List<int>> riskLevelMap) {
+  for (int i = 0; i < riskLevelMap.length; i++) {
+    for (int j = 0; j < riskLevelMap.length; j++) {}
+  }
+  return 0;
+}
+
+List<List<int>> getInput() {
+  var lines = File('day15/input.txt').readAsLinesSync();
+  List<List<int>> riskLevelMap = [];
+  for (var line in lines) {
+    riskLevelMap.add(line.split('').map((e) => int.parse(e)).toList());
+  }
+  return riskLevelMap;
 }
 
 void main() {
-  var lines = getInput();
+  var riskLevelMap = getInput();
+  var total = safestRoute(riskLevelMap);
+
+  print(total);
 }
